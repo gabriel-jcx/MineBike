9cb0e71c9e5db35ed0a6d90bf004031de4dbd2c9
YunHo Huh <yunhoh@uci.edu>
2015-11-06 19:01:29 -0800

    Added the Quest Statemachine

--- MinecraftMod/src/main/java/org/ngs/bigx/minecraft/quests/QuestStateManager.java
+++ MinecraftMod/src/main/java/org/ngs/bigx/minecraft/quests/QuestStateManager.java
@@ -1,17 +1,58 @@
 package org.ngs.bigx.minecraft.quests;
 
+import com.github.oxo42.stateless4j.StateMachine;
+
 public class QuestStateManager 
 {
-	public QuestStateManager()
-	{	
+	private StateMachine<State, Trigger> QuestStateMachine;
+	
+	public QuestStateManager() throws Exception
+	{
+		this.QuestStateMachine = new StateMachine<State, Trigger>(State.Inactive);
+		
+		this.QuestStateMachine.configure(State.Inactive)
+			.permit(Trigger.AcceptQuestAndTeleport, State.QuestLoading);
+		
+		this.QuestStateMachine.configure(State.QuestLoading)
+			.permit(Trigger.TeleportDone, State.WaitToStart);
+		
+		this.QuestStateMachine.configure(State.WaitToStart)
+			.permit(Trigger.StartQuest, State.QuestInProgress);
+		
+		this.QuestStateMachine.configure(State.QuestInProgress)
+			.permit(Trigger.PauseQuest, State.QuestPaused)
+			.permit(Trigger.SuccessQuest, State.QuestAccomplished)
+			.permit(Trigger.FailureQuest, State.QuestFailed);
+		
+		this.QuestStateMachine.configure(State.QuestPaused)
+			.permit(Trigger.ResumeQuest, State.QuestInProgress);
+		
+		this.QuestStateMachine.configure(State.QuestAccomplished)
+			.permit(Trigger.ScoreDisplayClickOkay, State.RewardSelection);
+		
+		this.QuestStateMachine.configure(State.RewardSelection)
+			.permit(Trigger.RewardSelect, State.RetryOrEndTheQuest);
+		
+		this.QuestStateMachine.configure(State.QuestFailed)
+			.permit(Trigger.ScoreDisplayClickOkay, State.RetryOrEndTheQuest);
+		
+		this.QuestStateMachine.configure(State.RetryOrEndTheQuest)
+			.permit(Trigger.NextgameRetryClick, State.QuestLoading)
+			.permit(Trigger.NextgameExitQuestClick, State.Inactive);
 	}
 	
 	private enum State {
-        Dead, Idle, QuestLoading, WaitToBeReady, QuestInProgress, QuestAccomplished, QuestFailed, RewardSelection, RetryOrEndTheQuest
+        //Dead, Idle,
+		Inactive, 
+        QuestLoading, WaitToStart, QuestInProgress, 
+        QuestPaused, QuestAccomplished, QuestFailed, RewardSelection, 
+        RetryOrEndTheQuest
 
     }
     private enum Trigger {
-        Die, Revive, AcceptQuest, 
-
+        //Die, Revive,
+    	AcceptQuestAndTeleport, TeleportDone, StartQuest, 
+        PauseQuest, StopQuest, ResumeQuest, SuccessQuest, FailureQuest, RewardSelect,
+        ScoreDisplayClickOkay, NextgameRetryClick, NextgameExitQuestClick
     }
 }
