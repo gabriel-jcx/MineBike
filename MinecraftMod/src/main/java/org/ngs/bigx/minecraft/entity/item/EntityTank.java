7aac102cbb889494905bc4cbe4d859f321f2f1da
Yunho Huh <yunhoh@uci.edu>
2016-05-24 21:34:52 -0700

    Finally the tank is movable!!! But need to disable AI...

--- MinecraftMod/src/main/java/org/ngs/bigx/minecraft/entity/item/EntityTank.java
+++ MinecraftMod/src/main/java/org/ngs/bigx/minecraft/entity/item/EntityTank.java
@@ -3,31 +3,25 @@ package org.ngs.bigx.minecraft.entity.item;
 import org.ngs.bigx.minecraft.Main;
 
 import cpw.mods.fml.common.registry.EntityRegistry;
-import net.minecraft.entity.EntityAgeable;
 import net.minecraft.entity.EntityList;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.EnumCreatureType;
-import net.minecraft.entity.ai.EntityAIControlledByPlayer;
-import net.minecraft.entity.passive.EntityAnimal;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
 
-public class EntityTank extends EntityAnimal {
-
-	/** AI task for player control. */
-    private final EntityAIControlledByPlayer aiControlledByPlayer;
-	
-	
+public class EntityTank extends EntityMob 
+{
 	public EntityTank(World p_i1712_1_)
     {
         super(p_i1712_1_);
         this.setSize(1.0F, 1.0F);
-        this.tasks.addTask(0, this.aiControlledByPlayer = new EntityAIControlledByPlayer(this, 0.3F));
         
     }
 	
-
-
 	public static void mainRegistry(){
 		registerEntity();
 	}
@@ -51,52 +45,59 @@ public class EntityTank extends EntityAnimal {
 	
 	}
 	
-	
-	protected boolean isAIEnabled()
-	{
-	    return true;
-	}
-	
-	protected void updateAITasks()
-    {
-        super.updateAITasks();
-    }
-	
-	public boolean canBeSteered()
-    {
-		return true;
-    }
-	
-	@Override
-	public EntityAgeable createChild(EntityAgeable p_90011_1_) {
-		return new EntityTank(this.worldObj);
-	}
-	
 	/**
      * Called when a player interacts with a mob. e.g. gets milk from a cow, gets into the saddle on a pig.
      */
-    public boolean interact(EntityPlayer p_70085_1_)
-    {
-        if (this.riddenByEntity == null || this.riddenByEntity == p_70085_1_)
-        {
-            p_70085_1_.mountEntity(this);
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+	public boolean interact(EntityPlayer entityplayer)
+	{
+		if (riddenByEntity == null || riddenByEntity == entityplayer)
+		{
+			entityplayer.mountEntity(this);
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
 	
-
-	  
-
-	  /**
-	     * Return the AI task for player control.
-	     */
-	    public EntityAIControlledByPlayer getAIControlledByPlayer()
-	    {
-	        return this.aiControlledByPlayer;
-	    }
-
+	public void moveEntityWithHeading(float p_70612_1_, float p_70612_2_)
+	{
+		if (this.riddenByEntity != null && this.riddenByEntity instanceof EntityLivingBase)
+		{
+			this.prevRotationYaw = this.rotationYaw = this.riddenByEntity.rotationYaw;
+			this.rotationPitch = this.riddenByEntity.rotationPitch * 0.5F;
+			this.setRotation(this.rotationYaw, this.rotationPitch);
+			this.rotationYawHead = this.renderYawOffset = this.rotationYaw;
+			p_70612_1_ = ((EntityLivingBase)this.riddenByEntity).moveStrafing * 0.1F; //initial value 0.5F
+			p_70612_2_ = ((EntityLivingBase)this.riddenByEntity).moveForward * 0.50F; //Forward move speed
+			if (p_70612_2_ <= 0.0F)
+			{
+				p_70612_2_ *= 0.25F; //Reverse move speed
+			}
+			this.stepHeight = 1.0F;
+			this.jumpMovementFactor = this.getAIMoveSpeed() * 0.1F;
+			if (!this.worldObj.isRemote)
+			{
+				this.setAIMoveSpeed((float)this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).getAttributeValue());
+				super.moveEntityWithHeading(p_70612_1_, p_70612_2_);
+			}
+			this.prevLimbSwingAmount = this.limbSwingAmount;
+			double d1 = this.posX - this.prevPosX;
+			double d0 = this.posZ - this.prevPosZ;
+			float f4 = MathHelper.sqrt_double(d1 * d1 + d0 * d0) * 4.0F;
+			if (f4 > 1.0F)
+			{
+				f4 = 1.0F;
+			}
+			this.limbSwingAmount += (f4 - this.limbSwingAmount) * 0.4F;
+			this.limbSwing += this.limbSwingAmount;
+		}
+		else
+		{
+			this.stepHeight = 0.5F;
+			this.jumpMovementFactor = 0.02F;
+			super.moveEntityWithHeading(p_70612_1_, p_70612_2_);
+		}
+	}
 }
