703a47cbc3b7994f1aa608b9496869d2e0d56c56
Yunho Huh <yunhoh@uci.edu>
2016-08-29 12:32:40 -0700

    Started adding/editing property classes.

--- MinecraftMod/src/main/java/org/ngs/bigx/minecraft/entity/lotom/BackpackProperty.java
+++ MinecraftMod/src/main/java/org/ngs/bigx/minecraft/entity/lotom/BackpackProperty.java
@@ -1,8 +1,95 @@
 package org.ngs.bigx.minecraft.entity.lotom;
 
+import org.ngs.bigx.minecraft.BiGXConnectionStateManagerClass.connectionStateEnum;
+
 public class BackpackProperty {
+	public static enum backpackType {
+		small(0), medium(1), large(2);
+		
+		private final int value;
+		private backpackType(int value) {
+			this.value = value;
+		}
+		
+		public int getValue() {
+			return value;
+		}
+		
+		public static backpackType fromInt(int i) {
+			for (backpackType b : backpackType.values()) {
+	            if (b.getValue() == i) { return b; }
+	        }
+	        return null;
+		}
+	};
+	public static final int[] backpackMaxVolumeByType = {16, 64, 256};
+	public static final int[] backpackWeightByType = {1, 3, 9};
+	
 	private boolean isActivated = false;	// Initially the player has only on backpack
-	private short backpackSie = 1;
-	private Object 
+	private backpackType backpackSize = backpackType.small;
+	private Object[] items;
+	private int weight;
+	private int volumeMax;
+	
+	public void init()
+	{
+		this.isActivated = false;
+		this.items = new Object[256];
+		this.initToSize(backpackType.small);
+	}
+	
+	public void initToSize(backpackType type)
+	{
+		int typeInInt = type.getValue();
+		
+		this.backpackSize = type;
+		this.volumeMax = backpackMaxVolumeByType[typeInInt];
+		this.weight = backpackWeightByType[typeInInt];
+	}
+
+	public boolean isActivated() {
+		return isActivated;
+	}
+
+	public void activateBackpack() {
+		this.isActivated = true;
+	}
+
+	public void deactivateBackpack() {
+		this.isActivated = false;
+	}
+
+	public backpackType getBackpackSize() {
+		return backpackSize;
+	}
+
+	public void setBackpackSize(backpackType backpackSize) {
+		this.backpackSize = backpackSize;
+	}
+
+	public Object[] getItems() {
+		return items;
+	}
+
+	public void setItems(Object[] items) {
+		this.items = items;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public int getVolumeMax() {
+		return volumeMax;
+	}
+
+	public void setVolumeMax(int volumeMax) {
+		this.volumeMax = volumeMax;
+	}
+	
 	
 }
--- MinecraftMod/src/main/java/org/ngs/bigx/minecraft/entity/lotom/CharacterProperty.java
+++ MinecraftMod/src/main/java/org/ngs/bigx/minecraft/entity/lotom/CharacterProperty.java
@@ -4,7 +4,7 @@ public class CharacterProperty {
 	private double oxygenLevel = 100;
 	private double oxygenCosumptionRate = 0.1;	// Oxygen consumption rate per 50 milliseconds
 	private double oxygenLevelMax = 100;		// Default amount of oxgygen a player can hold without any equipment
-	private double weight = 60; 				// The unit is Kg
+	private int weight = 60; 					// The unit is Kg
 	private double speedRate = 1;
 	private int strength = 10;					// Abstracted strength: Pedaling Resistance down
 	private int skill = 10;						// Abstracted skill: Skill to craft items
@@ -21,6 +21,5 @@ public class CharacterProperty {
 		this.strength = 10;
 		this.skill = 10;
 		this.luck = 10;
-		this.backpacks[0].
 	}
 }
