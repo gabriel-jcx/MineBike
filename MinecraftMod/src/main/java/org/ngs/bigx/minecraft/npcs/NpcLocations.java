800a7959fadb872ac278db8fd55da5f482852b35
Crystal Agerton <crystal.agerton2015@gmail.com>
2017-04-14 23:51:33 -0700

    Modified NPC and chest locations

--- MinecraftMod/src/main/java/org/ngs/bigx/minecraft/npcs/NpcLocations.java
+++ MinecraftMod/src/main/java/org/ngs/bigx/minecraft/npcs/NpcLocations.java
@@ -4,7 +4,7 @@ import net.minecraft.util.Vec3;
 
 
 public class NpcLocations {
-	public static Vec3 dad= Vec3.createVectorHelper(85, 73, 180);
-	public static Vec3 weaponsMerchant = Vec3.createVectorHelper(116, 73, 202);
-	public static Vec3 blacksmith = Vec3.createVectorHelper(116, 73, 196);
+	public static Vec3 dad= Vec3.createVectorHelper(85, 71, 180);
+	public static Vec3 weaponsMerchant = Vec3.createVectorHelper(116, 71, 202);
+	public static Vec3 blacksmith = Vec3.createVectorHelper(116, 71, 196);
 }
