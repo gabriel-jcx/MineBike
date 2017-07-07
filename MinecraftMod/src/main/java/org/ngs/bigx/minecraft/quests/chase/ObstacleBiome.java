6d1beb6b6aa3ec1e42bfaf6d22fabe4963a1603d
yunhoh@uci.edu <yunhoh@uci.edu>
2017-07-07 15:43:11 -0700

    Added new files!!!

---
+++ MinecraftMod/src/main/java/org/ngs/bigx/minecraft/quests/chase/ObstacleBiome.java
@@ -0,0 +1,42 @@
+package org.ngs.bigx.minecraft.quests.chase;
+
+import net.minecraft.init.Blocks;
+
+public class ObstacleBiome {
+	public TerrainBiomeDef obstacleBiomeDef;
+	
+	public ObstacleBiome()
+	{
+		this.obstacleBiomeDef = new TerrainBiomeDef();
+		
+		this.createObstacleBiome();
+	}
+	
+	public TerrainBiomeArea getObstacleBiomeByIdex(int index)
+	{
+		if(this.obstacleBiomeDef == null)
+			return null;
+		
+		if(this.obstacleBiomeDef.areas == null)
+			return null;
+		
+		if(this.obstacleBiomeDef.areas.size() >= index)
+			return null;
+		
+		return this.obstacleBiomeDef.areas.get(index);
+	}
+	
+	public void createObstacleBiome()
+	{
+		TerrainBiomeArea biomeArea = new TerrainBiomeArea();
+		
+		// SAMPLE
+		biomeArea = new TerrainBiomeArea();
+		biomeArea.map.put(new TerrainBiomeAreaIndex(1,0,2,2), Blocks.sandstone_stairs);
+		biomeArea.map.put(new TerrainBiomeAreaIndex(1,0,4,3), Blocks.sandstone_stairs);
+		biomeArea.map.put(new TerrainBiomeAreaIndex(3,0,2,2), Blocks.sandstone_stairs);
+		biomeArea.map.put(new TerrainBiomeAreaIndex(3,0,4,3), Blocks.sandstone_stairs);
+
+		this.obstacleBiomeDef.areas.add(biomeArea);
+	}
+}
---
+++ MinecraftMod/src/main/java/org/ngs/bigx/minecraft/quests/interfaces/IQuestEventItemPickUp.java
@@ -0,0 +1,7 @@
+package org.ngs.bigx.minecraft.quests.interfaces;
+
+import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
+
+public interface IQuestEventItemPickUp {
+	public void onItemPickUp(EntityItemPickupEvent event);
+}
