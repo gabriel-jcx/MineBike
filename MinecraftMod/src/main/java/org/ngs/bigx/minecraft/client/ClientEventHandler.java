340c369757c4a20410102c264cf7aeae7ad04903
yunhoh@uci.edu <yunhoh@uci.edu>
2017-12-18 17:01:47 -0800

    Added Resistance Changes Tagging. Updated the Dictionary.

--- MinecraftMod/src/main/java/org/ngs/bigx/minecraft/client/ClientEventHandler.java
+++ MinecraftMod/src/main/java/org/ngs/bigx/minecraft/client/ClientEventHandler.java
@@ -131,6 +131,33 @@ public class ClientEventHandler implements IPedalingComboEvent {
 			event.entity.motionY = 0;
 	}
 	
+	public static void sendResistanceGameTag(int resistanceLevel)
+	{
+		// SEND GAME TAG - Quest 0x(GAME TAG[0xFF])(questActivityTagEnum [0xF])
+		try {
+			int resistanceTypeEnum = (0xfff & resistanceLevel);
+			BiGXGameTag biGXGameTag = new BiGXGameTag();
+			biGXGameTag.setTagName("" + (Specification.GameTagType.GAMETAG_ID_RESISTANCE_BEGINNING | resistanceTypeEnum));
+			
+			BigxClientContext.sendGameTag(biGXGameTag);
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BiGXNetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BiGXInternalGamePluginExcpetion e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	@SideOnly(Side.CLIENT)
 	@SubscribeEvent(priority=EventPriority.NORMAL, receiveCanceled=true)
 	public void onEvent(KeyInputEvent event) 
@@ -686,6 +713,8 @@ public class ClientEventHandler implements IPedalingComboEvent {
 				BiGXNetPacket packet = new BiGXNetPacket(org.ngs.bigx.dictionary.protocol.Specification.Command.REQ_SEND_DATA, 0x0100, 
 						org.ngs.bigx.dictionary.protocol.Specification.DataType.RESISTANCE, buf.array());
 				BiGXPacketHandler.sendPacket(context.bigxclient, packet);
+
+				sendResistanceGameTag((int)new_resistance);
 			}
 		}
 	}
